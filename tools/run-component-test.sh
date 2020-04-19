7b8b3ba5b73dc88e4f47506126ba22a4121ab897
Bjarne Michelsen <bjarne_michelsen@hotmail.com>
2020-04-19 12:59:45 +0200

    chore: refine run-component-test script

--- tools/run-component-test.sh
+++ tools/run-component-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 SRCREV="$(git ls-remote origin | head -1 | cut -f 1)"
 COMPONENT_NAME=$1
@@ -11,27 +11,29 @@ PROJECT_PATH="/home/bjarne/repos/example-ptest"
 # Ensure devtool is available?
 # boot up a qemu if not already running
 setup() {
-	pushd ${YOCTO_PATH}
+	pushd ${YOCTO_PATH} > /dev/null
 	if ! [ -x "$(command -v devtool)" ]; then
-		source poky/oe-init-build-env
-		echo 'Error: cound not find `devtool`. did you forgot to source?' >&2
+		source poky/oe-init-build-env 2>&1 > /dev/null
+		#echo 'Error: cound not find `devtool`. did you forgot to source?' >&2
 	fi
 	if ! [ -x "$(pgrep -x "qemu")" ]; then
 		echo "Starting qemu"
 		# TODO: boot qemu in background
 		# take a look at testimage.bbclass
-		runqemu ${IMAGE} qemuparams='--nographic'
+		#runqemu ${IMAGE} qemuparams='--nographic' &
 	fi
 	echo "Updating recipe to build from local branch ${BRANCH_NAME}"
 	# TODO: what if the sources are already in workspace?
 	# 1. call devtool reset --remove-work
 	# 2. don't force anything, just fail with message.
 	devtool modify ${COMPONENT_NAME} --no-extract ${PROJECT_PATH} 2>&1 > /dev/null
-	popd
+	popd > /dev/null
 }
 
 build() {
-	devtool build ${COMPONENT_NAME} 2>&1 > /dev/null
+	# TODO: ensure build was successful, otherwise fail.
+	echo "Building ${COMPONENT_NAME}"
+	devtool build ${COMPONENT_NAME} 
 }
 
 run_test() {
@@ -39,7 +41,9 @@ run_test() {
 	# Host 192.168.7.2
         #   StrictHostKeyChecking no
 	# Otherwise deploy-target will fail
-	devtool deploy-target ${COMPONENT_NAME} ${TARGET}
+	echo "Deploying to qemu"
+	devtool deploy-target ${COMPONENT_NAME} ${TARGET} 2>&1 > /dev/null
+	echo "Running tests"
 	ssh ${TARGET} ptest-runner ${COMPONENT_NAME}
 }
 
@@ -51,6 +55,6 @@ update_recipe() {
 
 setup
 build
-[ $? -eq 0 ] || exit 1
 run_test
-update_recipe
+# Update recipe could be optional. Not necessary for testing.
+#update_recipe
