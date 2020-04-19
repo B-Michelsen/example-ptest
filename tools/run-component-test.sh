746bd5fdd9a4a59e565373f04bf97e81bc8f2896
Bjarne Michelsen <bjarne_michelsen@hotmail.com>
2020-04-19 15:04:48 +0200

    chore: add error handling in run-component-test script

--- tools/run-component-test.sh
+++ tools/run-component-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COMPONENT_NAME=$1
+COMPONENT=$1
 TARGET="root@192.168.7.2"
 IMAGE="core-image-full-cmdline"
 YOCTO_PATH="/home/bjarne/repos/yocto"
@@ -8,10 +8,12 @@ PROJECT_PATH="/home/bjarne/repos/example-ptest"
 # Ensure devtool is available?
 # boot up a qemu if not already running
 setup() {
+	local COMPONENT=$1
+	local PROJECT_PATH=$2
+	local YOCTO_PATH=$3
 	pushd ${YOCTO_PATH} > /dev/null
 	if ! [ -x "$(command -v devtool)" ]; then
-		source poky/oe-init-build-env 2>&1 > /dev/null
-		#echo 'Error: cound not find `devtool`. did you forgot to source?' >&2
+		source poky/oe-init-build-env ${YOCTO_PATH}/build 2>&1 > /dev/null
 	fi
 	if ! [ -x "$(pgrep -x "qemu")" ]; then
 		echo "Starting qemu"
@@ -19,31 +21,36 @@ setup() {
 		# take a look at testimage.bbclass
 		#runqemu ${IMAGE} qemuparams='--nographic' &
 	fi
-	echo "Updating recipe to build from repository ${PROJECT_PATH}"
-	# TODO: what if the sources are already in workspace?
-	# 1. call devtool reset --remove-work
-	# 2. don't force anything, just fail with message.
-	devtool modify ${COMPONENT_NAME} --no-extract ${PROJECT_PATH} 2>&1 > /dev/null
+	echo "Updating recipe ${COMPONENT} to build from repository ${PROJECT_PATH}"
+	devtool modify ${COMPONENT} --no-extract ${PROJECT_PATH} 2>&1 > /dev/null
 	popd > /dev/null
 }
 
 build() {
+	local COMPONENT=$1
 	# TODO: ensure build was successful, otherwise fail.
-	echo "Building ${COMPONENT_NAME}"
-	devtool build ${COMPONENT_NAME} 
+	echo "Building ${COMPONENT}"
+	devtool build ${COMPONENT} 
+	return $?
 }
 
 run_test() {
-        # TODO: add host as no host key checking in ~/.ssh/config
-	# Host 192.168.7.2
-        #   StrictHostKeyChecking no
-	# Otherwise deploy-target will fail
+	local AUT=$1
+	local TEST_TARGET=$2
 	echo "Deploying to qemu"
-	devtool deploy-target ${COMPONENT_NAME} ${TARGET} 2>&1 > /dev/null
-	echo "Running tests"
-	ssh ${TARGET} ptest-runner ${COMPONENT_NAME}
+	devtool deploy-target -cs ${AUT} ${TEST_TARGET} 2>&1 > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Running tests"
+		ssh ${TEST_TARGET} ptest-runner ${AUT}
+	else
+		echo "Failed to deploy, please check if qemu is running"
+	fi
 }
 
-setup
-build
-run_test
+setup ${COMPONENT} ${PROJECT_PATH} ${YOCTO_PATH}
+build ${COMPONENT}
+if [ $? -eq 0 ]; then
+	run_test ${COMPONENT} ${TARGET}
+else
+	echo "Failed to build target"
+fi
